91a2519b89ce1313287dba8e8ca8be0bcbad9798
Paul Keene <38261292+pkeene17@users.noreply.github.com>
2023-11-01 11:28:35 -0400

    Delete install_packages.sh
    remove old install_packages

--- install_packages.sh
+++
@@ -1,9 +0,0 @@
-python -m ensurepip --upgrade
-pip install certifi
-pip install requests
-pip install datetime
-pip install numpy
-pip install pandas
-pip install dplython
-pip install functools
-pip install ics
