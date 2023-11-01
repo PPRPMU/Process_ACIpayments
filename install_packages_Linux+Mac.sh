9eb8603e961641d0ec57aa8b41e2c7a0248aa1a7
Paul Keene <38261292+pkeene17@users.noreply.github.com>
2023-11-01 11:28:03 -0400

    update install_packages
    add a batch file for use on windows. Change file names to describe intended system.

---
+++ install_packages_Linux+Mac.sh
@@ -0,0 +1,9 @@
+python -m ensurepip --upgrade
+pip install certifi
+pip install requests
+pip install datetime
+pip install numpy
+pip install pandas
+pip install dplython
+pip install functools
+pip install ics
